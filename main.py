79fbe56136aa76135aa4cf4ef2ef01ff80f62e94
Rushil <51292937+rushil1904@users.noreply.github.com>
2021-06-16 11:01:54 +0000

    Added all API endpoints

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import requests
 import json
 
 base_api = 'https://api.wazirx.com/'
+#API Endpoints
 market_status = requests.get(base_api + '/api/v2/market-status').json()
 market_ticker = requests.get(base_api + '/api/v2/tickers').json()
-market_depth = requests.get(base_api + '/api/v2/depth').json()
-market_trade_history = requests.get(base_api + '/api/v2/trades').json()
+market_depth = requests.get(base_api + '/api/v2/depth?market={}'.format('btcinr')).json()
+market_trade_history = requests.get(base_api + '/api/v2/trades?market={}'.format('btcinr')).json()
 
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
@@ -20,3 +21,7 @@ if 'exchange' in response:
   doge = print("Doge: "), jprint(market_ticker['dogeinr'])
   matic = print("Matic: "), jprint(market_ticker['maticinr'])
   shibu = print("Shibu: "), jprint(market_ticker['shibinr'])
+
+if 'status' in response:
+  jprint(market_status['markets']), jprint(market_status['assets'])
+
